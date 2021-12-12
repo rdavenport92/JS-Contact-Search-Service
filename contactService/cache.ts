e6cc1e42fae8eee36b1d78af5fcd7b75032a82b6
Ricky Davenport <ricky.davenport@wwt.com>
2021-12-12 17:55:59 -0600

    add cache class

---
+++ contactService/cache.ts
@@ -0,0 +1,44 @@
+import { ContactID, IContactCacheContent, ICache, IContactDB } from '../types';
+
+export default class ContactCache implements ICache<IContactDB> {
+    private _content: IContactCacheContent = {};
+
+    constructor(initialContactCacheContent: IContactDB[] = []) {
+        this._initializeContactCache(initialContactCacheContent);
+    }
+
+    private _initializeContactCache(initialContactCacheContent: IContactDB[]) {
+        for (const currentContact of initialContactCacheContent) {
+            this.add(currentContact.id, currentContact);
+        }
+    }
+
+    get(id: ContactID) {
+        return !!this._content[id]
+            ? {
+                  ...this._content[id],
+              }
+            : null;
+    }
+
+    getAll() {
+        return Object.keys(this._content).map(
+            (itemKey) => this._content[itemKey]
+        );
+    }
+
+    add(id: ContactID, contact: IContactDB) {
+        this._content[id] = contact;
+        return this._content[id];
+    }
+
+    remove(id: ContactID) {
+        // TODO: implement
+        return {} as IContactDB;
+    }
+
+    update(id: ContactID, field: string, value: string) {
+        // TODO: implement
+        return {} as IContactDB;
+    }
+}
---
+++ test/contactService/cache.test.ts
@@ -0,0 +1,76 @@
+import { expect } from 'chai';
+import Chance from 'chance';
+import Contact from '../../accessLayer/model';
+
+import ContactCache from '../../contactService/cache';
+import { IContactDB } from '../../types';
+import uuid from '../../utilities/uuid';
+
+const chance = new Chance();
+
+const generateContacts = (
+    minContacts: number = 1,
+    maxContacts: number = 1
+): IContactDB[] =>
+    [...Array(chance.integer({ min: minContacts, max: maxContacts }))].map(
+        () => new Contact(uuid()) as IContactDB
+    );
+
+describe('Contact Cache', () => {
+    describe('getAll', () => {
+        it('should return all contacts in cache', () => {
+            const initialContacts = generateContacts(5, 15);
+            const testCache = new ContactCache(initialContacts);
+
+            const result = testCache.getAll();
+
+            expect(result).to.deep.equal(initialContacts);
+        });
+    });
+    describe('get', () => {
+        it('should retrieve a contact by id', () => {
+            const initialContacts = generateContacts();
+            const testId = initialContacts[0].id;
+            const testCache = new ContactCache(initialContacts);
+
+            const result = testCache.get(testId);
+
+            expect(result).to.deep.equal(initialContacts[0]);
+        });
+
+        it('should return null if contact does not exist for the specified id', () => {
+            const testCache = new ContactCache();
+
+            const result = testCache.get('invalid-id');
+
+            expect(result).to.be.null;
+        });
+    });
+    describe('add', () => {
+        it('should add a contact to cache with specified id as key', () => {
+            const testContact = generateContacts()[0];
+            const testId = testContact.id;
+            const testCache = new ContactCache();
+
+            testCache.add(testId, testContact);
+
+            expect(testCache.get(testId)).to.deep.equal(testContact);
+        });
+        it('should return the contact back to the caller', () => {
+            const testContact = generateContacts()[0];
+            const testId = testContact.id;
+            const testCache = new ContactCache();
+
+            const result = testCache.add(testId, testContact);
+
+            expect(result).to.deep.equal(testContact);
+        });
+    });
+    it('initializes with no contacts in cache if no initial value specified', () => {
+        const testCache = new ContactCache();
+
+        const result = testCache.getAll();
+
+        expect(result.length).equals(0);
+    });
+});
