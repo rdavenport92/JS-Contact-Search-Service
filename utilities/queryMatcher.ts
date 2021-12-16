7a25ee28b6a98394a4c6450fcfd4ad4171fb612f
Ricky Davenport <ricky.davenport@wwt.com>
2021-12-16 17:56:47 -0600

    create queryMatcher

---
+++ utilities/queryMatcher.ts
@@ -0,0 +1,66 @@
+import {
+    IQueryMatcherPlugin,
+    QueryObjFieldsT,
+    QueryMatcherMapT,
+    QueryMatcherT,
+    QueryObjectT,
+    QueryObjFieldT,
+} from '../types';
+
+const defaultMatcher = <T>(
+    query: string,
+    obj: QueryObjectT<T>,
+    field: string
+) => obj[field] instanceof String && (obj[field] as string).includes(query);
+
+const assignDefaultHandlers = <T>(
+    matcherMap: QueryMatcherMapT<T>,
+    currentField: QueryObjFieldT<T>,
+    defaultMatchHandler: QueryMatcherT<T>
+) =>
+    matcherMap[currentField]
+        ? matcherMap
+        : { ...matcherMap, [currentField]: defaultMatchHandler };
+
+const assignMatcherPluginHandlers = <T>(
+    matcherPlugins: IQueryMatcherPlugin<T>[],
+    initialMatcherMap: QueryMatcherMapT<T>
+) => {
+    for (const { applicableQueryFields, match: matchFn } of matcherPlugins) {
+        for (const entry of applicableQueryFields) {
+            initialMatcherMap[entry] = matchFn;
+        }
+    }
+
+    return initialMatcherMap;
+};
+
+export class QueryMatcherMapFactory {
+    static create = <T>(
+        objFields: QueryObjFieldsT<T>,
+        matcherPlugins: IQueryMatcherPlugin<T>[],
+        defaultMatchHandler: QueryMatcherT<T> = defaultMatcher
+    ): QueryMatcherMapT<T> =>
+        objFields.reduce((matcherMap, currentField) => {
+            return assignDefaultHandlers(
+                matcherMap,
+                currentField,
+                defaultMatchHandler
+            );
+        }, assignMatcherPluginHandlers(matcherPlugins, {} as QueryMatcherMapT<T>));
+}
+
+export default class QueryMatcher<T> {
+    _queryMatcherMap: QueryMatcherMapT<T>;
+
+    constructor(queryMatcherMap: QueryMatcherMapT<T>) {
+        this._queryMatcherMap = queryMatcherMap;
+    }
+
+    match = (query: string, obj: QueryObjectT<T>): boolean =>
+        !!Object.keys(obj).find((fieldToCheck) =>
+            obj[fieldToCheck]
+                ? this._queryMatcherMap[fieldToCheck](query, obj, fieldToCheck)
+                : false
+        );
+}
